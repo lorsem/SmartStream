06df875bf12384862ffcdf6c2d550582c1a6caa1
lorsem <lorsem@hotmail.it>
2014-03-14 00:26:48 +0100

    Working Server script

--- ServerScript.py
+++ ServerScript.py
@@ -4,37 +4,6 @@ import cgitb
 import os
 cgitb.enable()
 
-# Retrieve form fields
-form   = cgi.FieldStorage()			# Get POST data
-TheDirectory  = form.getfirst("TheDirectory")			# Pull fname field data
-Index = open('/var/www/list_of_films.txt', 'w')
-if TheDirectory.lower() == 'default':
-    TheDirectory = '/media/HDD'
-
-DirList = get_filepaths(TheDirectory)
-pos = 0
-while True:
-    try:
-        Index.write(DirList[Pos])
-        #Index.write('\n') NEEDED?
-    except IndexError:
-        break
-    pos += 1
-#output on the page of the script
-print '''
-
-
-Content-Type: text/html; charset=UTF-8
-<html>
-<body>
-Indexing completed!
-Found {0} films!
-<br>
-<a href="http://192.168.1.150/index.html"><b>HOME</b></a>
-</body>
-</html>
-'''.format(str(pos))
-
 
 def isMovie(filename):
     videoExtensions = ('.wmv', '.mov', '.mpg', '.avi', '.mp4', '.mkv', 'm4v', '.flv')
@@ -60,4 +29,40 @@ def get_filepaths(directory):
                 file_paths.append(filepath)  # Add it to the list.
 
     return file_paths  
+
+
+
+# Retrieve form fields
+form   = cgi.FieldStorage()			# Get POST data
+TheDirectory  = form.getfirst("TheDirectory")			# Pull fname field data
+Index = open('/var/www/list_of_films.txt', 'w')
+if TheDirectory.lower() == 'default':
+    TheDirectory = '/media/HDD'
+
+DirList = get_filepaths(TheDirectory)
+pos = 0
+while True:
+    try:
+        Index.write(DirList[pos])
+        Index.write('\n') NEEDED!
+    except IndexError:
+        break
+    pos += 1
+#output on the page of the script
+print "Content-Type: text/html; charset=UTF-8"
+print ''
+print '''
+
+<html>
+<body>
+Indexing completed! <br>
+Found <b>{0}</b> films!
+<br>
+Searched <b>{1}</b>
+<br>
+<a href="http://192.168.1.150/index.html"><b>HOME</b></a>
+</body>
+</html>
+'''.format(str(pos), TheDirectory)
+
 
