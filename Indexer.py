fce111d6e8c3c4eac6a40562a0efff76be571629
Ascanio Vimercati <Ascanio@MacBook-di-Ascanio.local>
2014-03-13 23:07:56 +0100

    Ascanio è un fottuto genio

--- Indexer.py
+++ Indexer.py
@@ -1,7 +1,29 @@
-#!/usr/bin/env python3
-
 import os
 
-def Index(RootDir):
-    pass
+def isMovie(filename):
+    videoExtensions = ('.wmv', '.mov', '.mpg', '.avi', '.mp4', '.mkv', 'm4v', '.flv')
+    if filename.endswith(videoExtensions):
+        return True
+    else: return False
+
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    Movie files will be selected and listed.
+    """
+    file_paths = []  # List which will store all of the full filepaths (filename and extension included)
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            if isMovie(filename):    
+                # Join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  
+
 
