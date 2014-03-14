984edce8d75e12ea4fbd430fb8fc1bb5160270f1
lorsem <lorsem@hotmail.it>
2014-03-14 16:15:29 +0100

    Minor edits

--- ServerScript.py
+++ ServerScript.py
@@ -6,30 +6,35 @@ cgitb.enable()
 import os
 
 def isMovie(filename):
-    videoExtensions = ('.wmv', '.mov', '.mpg', '.avi', '.mp4', '.mkv', 'm4v', '.flv')
+    """
+    If it is a movie it returns the filename without the extension (to be used in index file)
+    else return False
+    """
+    videoExtensions = ('.wmv', '.mov', '.mpg', '.avi', '.mp4', '.mkv', 'm4v', '.flv') # Tuple containing supported video extensions
     if filename.endswith(videoExtensions):
-        return True
+        return os.path.splitext(filename)[0] # splitext return a list: [name, extension] 
     else: return False
 
-def get_filepaths(directory):
+def getIndex(directory):
     """
     os.walk() is awesome. Nothing more to say...
     The tupla returned gets unpaked, files contains filenames while root contains the filepath from passed
     argument to dile diretctory.
-    Movies are selected and listed with their full filepaht (e.g. directory/subDirectory/movie.extension)
+    It returns a dictionary, key = file name without extension, value = full file path with extension
     """
-    file_paths = []  # List which will store all of the full filepaths (filename and extension included)
+    index = {}
 
     # Walk the tree.
     for root, directories, files in os.walk(directory):
         for filename in files:
-            if isMovie(filename):    
+            name = isMovie(filename) # False if not a movie
+            if  name:    
                 # Join the two strings in order to form the full filepath.
                 filepath = os.path.join(root, filename)
-                file_paths.append(filepath)  # Add it to the list.
+                relFilePath = os.path.relpath(filepath, directory) # relpath returns releative path of first argument
+                index[name] = relFilePath  # Adds new item to dictionary
 
-    return file_paths  
-
+    return index  
 
 
 
