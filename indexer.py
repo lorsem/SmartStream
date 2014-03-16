2a2abf3615caed57b16182daa7a0172158e0afbf
AscanioFX <ascanio.vimercati@gmail.com>
2014-03-16 19:03:35 +0100

    Added comments to indexer.py

--- indexer.py
+++ indexer.py
@@ -9,6 +9,7 @@ If it is a movie it returns the filename without the extension
     """
     videoExtensions = ('.wmv', '.mov', '.mpg', '.avi', '.mp4', '.mkv', 'm4v',
                        '.flv')  # Tuple containing supported video extensions
+    # os.path.splitext() splits a string separating a .extension
     name, ext = os.path.splitext(filename)
     if ext in videoExtensions:
         return name
@@ -32,20 +33,40 @@ The Function returns a dictionary, key = file name without extension, value = \
 full file path with extension, relative to refDir
 
 DIR LISTING:
-    os.dirlist(d) retruns a list of what is in d. Just names. Since we need to know what is a directory and what is not we call os.path.isdir(d)
-    wher d MUST b a path, not just a name of a directory.
+    os.dirlist(d) retruns a list of what is in d. Just names. Since we need to know what is a directory 
+    and what's not we call os.path.isdir(d) where d MUST be a path, not just the name of a directory.
+    See "Future improvememnt" to see how thath could have been easily solved... 
+    
     dirDict is a dictionary: key = name, value = abs path
-    dirList contains dirDict keys, therefore it is a list of all the elements in the scanned directory. It is what os.dirlist would have returned.
+    dirList contains dirDict keys, therefore it is a list of all the elements in the scanned directory.
+    It is what os.dirlist would have returned. We don't actually need it....
+
+FUTURE IMPROVEMENTS
+    dirList can be obviously avoided....
+    Another level of optimization could be: having just a list of the elements in the dir (instead of a dictionary of
+    their names and paths) and use os.path.join(os.getcwd(), elementName) to construct it's absolute
+    path and use it to call os.isdir .... 
+    This way we would only have a list of the elements in the directory, listed with os.listdir and than 
+    we would be able to check directories by their name...
+    This was probably a way better and simpler way to go instead of the dict way
+
     """
     index = {}
+    # dirDict is a dictionary mapping names of elements in scanDir to abs path
     dirDict = {key : os.path.join(scanDir, key) for key in os.listdir(scanDir)}
+    # dirList is a list to iterates sorted keys. SACRIFICABILE in una futura ottimizzazione più pythonica
     dirList = dirDict.keys()
     for elem in sorted(dirList):
+        # name = FALSE if not a movie, movie-name cleaned from extenion if a movie
         name = isMovie(elem)
         if name:
             index[name] = os.path.relpath(dirDict[elem], refDir)
+        # if it's a directory... isdir requires the path of the directory, not just it's name...
         elif os.path.isdir(dirDict[elem]):
+            # tempIndex is gonna be the subDirectory, we need to use another name rather than "index" not to reassign it
+            # and loosing what's so far
             tempIndex = getIndex(dirDict[elem], refDir)
+            # only store non-empty direcories
             if tempIndex != {}:
                 index[elem] = tempIndex
     return index
