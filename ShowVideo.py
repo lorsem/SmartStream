8f82e1e9c9d784ea58676e54c2ec71bc6b7d838b
lorsem <lorsem@hotmail.it>
2014-03-18 18:27:24 +0100

    Added loading gif (while movie is not loaded)

--- ShowVideo.py
+++ ShowVideo.py
@@ -23,7 +23,7 @@ print '''
                 <body>
                 <center>File: {0}</center>
                 
-                <video width="1920" height="1080" controls autoplay>
+                <video width="1920" height="1080" controls autoplay poster="/wallpapers/loading.gif">
                   <source src="{1}">
                 </video>
                 <br><br>
