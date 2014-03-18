7e6a5661a43e43906c79439fd1a0091ba3f0bfed
lorsem <lorsem@hotmail.it>
2014-03-18 17:26:13 +0100

    IMPORTANT Corrected error. Now mkv should play OOTB in chrome and maybe safari

--- ShowVideo.py
+++ ShowVideo.py
@@ -24,11 +24,11 @@ print '''
                 <center>File: {0}</center>
                 
                 <video width="1920" height="1080" controls autoplay>
-                  <source src="{1}" type="{2}">
+                  <source src="{1}">
                 </video>
                 <br><br>
                 <a href="/FilmIndex.html">Film Index</a>
                 <br>
                 </body>
                 </html>
-'''.format(VidName, 'http://192.168.1.150' + Path, 'video/' + Path.split('.')[-1])
+'''.format(VidName, 'http://192.168.1.150' + Path)
