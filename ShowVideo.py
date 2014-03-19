3b60bd971f9f636e98f8edd6ad35a90718c42177
lorsem <lorsem@hotmail.it>
2014-03-20 00:09:05 +0100

    Greatly improved aesthetics

--- ShowVideo.py
+++ ShowVideo.py
@@ -18,17 +18,26 @@ print ''
 print '''
                 <html>
                 <head>
-                <title>SmartStream: {0}</title>
-                </head>
-                <body>
-                <center>File: {0}</center>
+                <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
+                <link rel="SHORTCUT ICON" HREF="wallpapers/icon_small.png">
+                <style type="text/css">
+                body {{background-color:black;
+                    background-image:none}}
+                </style>
                 
+                </head>
+                    <body>
+                    <center>
+                    <a href="/FilmIndex.html"><font color="#8D8D8D"><font size="4">Film Index</font></font></a>
+                    </center>
+                
+                <center>
                 <video width="1280" height="720" controls autoplay poster="/wallpapers/loading.gif">
                   <source src="{1}">
                 </video>
-                <br><br>
-                <a href="/FilmIndex.html">Film Index</a>
+                </center>
+                
                 <br>
                 </body>
                 </html>
-'''.format(VidName, 'http://192.168.1.150' + Path)
+'''.format(VidName, 'http://192.168.1.150' + Path) ##Double curled braces to escape python :)
