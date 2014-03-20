a4e7add6b8c068a471d57d13468e4de1844aac44
sco93 <pscorso93@gmail.com>
2014-03-20 09:31:59 +0100

    no need to escape in triple quotes

--- ShowVideo.py
+++ ShowVideo.py
@@ -16,28 +16,27 @@ Path = PartialPath
 print "Content-Type: text/html; charset=UTF-8"
 print ''
 print '''
-                <html>
-                <head>
-                <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
-                <link rel="SHORTCUT ICON" HREF="wallpapers/icon_small.png">
-                <style type="text/css">
-                body {{background-color:black;
-                    background-image:none}}
-                </style>
-                
-                </head>
-                    <body>
-                    <center>
-                    <a href="/FilmIndex.html"><font color="#8D8D8D"><font size="4">Film Index</font></font></a>
-                    </center>
-                
-                <center>
-                <video width="1280" height="720" controls autoplay poster="/wallpapers/loading.gif">
-                  <source src="{1}">
-                </video>
-                </center>
-                
-                <br>
-                </body>
-                </html>
-'''.format(VidName, 'http://192.168.1.150' + Path) ##Double curled braces to escape python :)
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
+        <link rel="SHORTCUT ICON" HREF="wallpapers/icon_small.png">
+        <style type="text/css">
+            body {background-color:black;
+                    background-image:none}
+        </style>
+    </head>
+    <body>
+        <center>
+            <a href="/FilmIndex.html"><font color="#8D8D8D"><font size="4">Film Index</font></font></a>
+        </center>
+
+        <center>
+            <video width="1280" height="720" controls autoplay poster="/wallpapers/loading.gif">
+                <source src="{1}">
+            </video>
+        </center>
+
+        <br>
+    </body>
+</html>
+'''.format(VidName, 'http://192.168.1.150' + Path)  # Double curled braces to escape python :)
