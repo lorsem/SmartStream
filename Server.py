2bf6b1179ae76bd49f284963da6cfbd55a814b05
sco93 <pscorso93@gmail.com>
2014-03-20 09:33:14 +0100

    no need to escape { in triple quotes

--- Server.py
+++ Server.py
@@ -4,49 +4,45 @@ import cgitb
 import indexer
 import CreateFilmIndex
 
-cgitb.enable()
-# Retrieve form fields
-form = cgi.FieldStorage()			# Get POST data
-scanDir = form.getfirst("scanDir")			# Pull fname field data
+cgitb.enable()                       # Retrieve form fields
+form = cgi.FieldStorage()			 # Get POST data
+scanDir = form.getfirst("scanDir")	 # Pull fname field data
 Index = open('/var/www/list_of_films.txt', 'w')
 if scanDir is None:
     scanDir = '/var/www/video'
 refDir = '/var/www'
 TheIndex = indexer.getIndex(scanDir, refDir)
 total = indexer.printIndex(TheIndex)
-indexer.store_index(TheIndex) #Quite useless nut not a performance issue
-#output on the page of the script
-
+indexer.store_index(TheIndex)   # Quite useless nut not a performance issue
+                                # output on the page of the script
 CreateFilmIndex.IndexEverything(TheIndex)
 
-
 print "Content-Type: text/html; charset=UTF-8"
-print ''
 print '''
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
     <style type="text/css">
-        a:link {{
+        a:link {
         color : white ;
         background-color:transparent;
         text-decoration:none;     /* no underline */
-    }}
-    a:visited {{
+    }
+    a:visited {
         color : white ;
         background-color:transparent;
         text-decoration:none;     /* no underline */
-    }}
-    a:hover {{
+    }
+    a:hover {
         color : white ;
         background-color:transparent;
         text-decoration:none;     /* no underline */
-    }}
-    a:active {{
+    }
+    a:active {
         color : white ;
         background-color:transparent;
         text-decoration:none;
-    }}
+    }
     </style>
     <link rel="SHORTCUT ICON" HREF="/wallpapers/icon_small.png">
 <title>SmartStream : Indexer</title>
@@ -57,7 +53,7 @@ print '''
                 <div id="logo">
                 </div>
 <br><center><font color="White" size="4">Indexing completed!  <br>
-Found <b>{0}</b> films! 
+Found <b>{0}</b> films!
 <br></font><font color="#FFFFFF" size="3">
 Searched <b>{1}</b></font></center>
 <br>
@@ -70,4 +66,4 @@ Searched <b>{1}</b></font></center>
 <br>
 </body>
 </html>
-'''.format(str(total), scanDir) #TheIndex
+'''.format(str(total), scanDir)  # TheIndex
