ab728aac14c9d30f0ee8a229bafaa2877f35ce3f
lorsem <lorsem@hotmail.it>
2014-03-20 22:34:46 +0100

    Good looking rendered page

--- fileUpload.py
+++ fileUpload.py
@@ -28,10 +28,56 @@ if fileitem.filename:
    
 else:
    message = 'No file was uploaded'
-   
-print """\
-Content-Type: text/html\n
-<html><body>
-<p>%s</p>
-</body></html>
-""" % (message,)
+
+
+print "Content-Type: text/html; charset=UTF-8"
+print """
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
+    <style type="text/css">
+        a:link {{
+        color : white ;
+        background-color:transparent;
+        text-decoration:none;     /* no underline */
+    }}
+    a:visited {{
+        color : white ;
+        background-color:transparent;
+        text-decoration:none;     /* no underline */
+    }}
+    a:hover {{
+        color : white ;
+        background-color:transparent;
+        text-decoration:none;     /* no underline */
+    }}
+    a:active {{
+        color : white ;
+        background-color:transparent;
+        text-decoration:none;
+    }}
+    </style>
+    <link rel="SHORTCUT ICON" HREF="/wallpapers/icon_small.png">
+<title>SmartStream : Uploader</title>
+</head>
+<body>
+<a href="/">
+        <img src="/wallpapers/Logo.png" alt="SmartStream" /></a>
+                <div id="logo">
+                </div>
+<br><center><font color="White" size="4">Upload completed!  <br>
+{0}
+<br></font><font color="#FFFFFF" size="3">
+Uploaded to:  <b>{1}</b></font></center>
+<br>
+<br>
+<center>
+<font color="White" size="5">
+<a href="http://192.168.1.150/index.html"><b>HOME</b></a>
+</font>
+</center>
+<br>
+</body>
+</html>
+
+""".format(message, reldir)
