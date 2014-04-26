d87ee1d36e25583ef557d524d03e0a784d2d9150
lorsem <lorsem@hotmail.it>
2014-04-26 23:31:54 +0200

    Needs vlc plugin (linux). Opens pretty much anything!

---
+++ Show__BETA.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+'''
+Given a relative path (relative to the root of the website), the video is nicely
+embedded in HTML5. The VidName is needed only to have a better output.
+'''
+import cgi
+import cgitb
+
+cgitb.enable()
+
+Fields = cgi.FieldStorage()
+PartialPath = Fields.getvalue('VidPath')
+VidName = Fields.getvalue('VidName')
+Path = PartialPath
+
+print "Content-Type: text/html; charset=UTF-8"
+print ''
+print '''
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/PageStyle.css" />
+        <link rel="SHORTCUT ICON" HREF="wallpapers/icon_small.png">
+        <style type="text/css">
+            body {{background-color:black;
+                    background-image:none}}
+        </style>
+        <title>SmartStream::Playback</title>
+    </head>
+    <body>
+        <center>
+            <a href="/index.html"><font color="#8D8D8D"><font size="4">
+            Film Index</font></font></a>
+        </center>
+        <center>
+            <font color="#8D8D8D"><font size="3">
+            Video: {0}</font></font>
+        </center>
+        <center>
+
+    <object type="application/videolan-vlc" data="{1}" width="1280" height="720" id="Video">
+         <param name="movie" value="{1}"/>
+         <embed type="application/videolan-vlc" name="video1"
+         autoplay="no" loop="no" width="1280" height="720"
+         target="{1}" />
+         <a href="{1}">Download Video1</a>
+    </object>
+
+        </center>
+
+        <br>
+    </body>
+</html>
+'''.format(VidName,  Path)  # Double curled braces to escape python :)  'http://192.168.1.150' +
+
+
+
+
+
+
+#
+#
+#
+#<object type="application/videolan-vlc" data="{1}" width="1280" height="720" id="Video">
+#     <param name="movie" value="{1}"/>
+#     <embed type="application/videolan-vlc" name="video1"
+#     autoplay="no" loop="no" width="1280" height="720"
+#     target="{1}" />
+#     <a href="{1}">Download Video1</a>
+#</object>
