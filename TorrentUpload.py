299899304e0a6d5d38bbb965e6d979700b510d6d
lorsem <lorsem@hotmail.it>
2014-04-29 23:35:39 +0200

    Added explanatory comments

--- TorrentUpload.py
+++ TorrentUpload.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
-import cgi
+import cgi   # The file is uploaded through a POST form (see
+             #  "TorrentUploadInterface.html" for an example)
 import os
-import cgitb
-import time
+import cgitb 
+import time  # Used for measuring time passed from beginning of execution
 
 cgitb.enable()
 
 
-# Generator to buffer file chunks
-
-start = time.time()
-uploadFlag = 0
+start = time.time() 
+uploadFlag = 0  # See below (for loop->try statement);its sole purpose is to try
+                #   and upload a single file only once. If the user uploads more
+                #   than a single file, files are in an array/list and the
+                #   IndexError exception is caught (for loop->except). If there
+                #   is just one file, TypeError exception is raised and the file
+                #   must be uploaded just once
 form = cgi.FieldStorage()
 message = str()
 # A nested FieldStorage instance holds the file
@@ -19,23 +23,25 @@ message = str()
 for n in range(100):
     try:
         fileitem = form['file'][n] #USE THIS says reddit form['file'].fil
-    except TypeError:
-        if uploadFlag == 0:
-            uploadFlag = 1
+    except TypeError:  # Happens if we have a single file instead of multiple files.
+                       # If there is only one file -> no array, but single file descriptor
+        if uploadFlag == 0: # We have only one file. Is it the 1st time we copy it to the disk?
+            uploadFlag = 1  # Set the flag, we have our file and will copy it later on
             fileitem = form['file']
-        else:
+        else:       # We have already uploaded our file
             break
-    except:
+    except: # We had multiple files and have copied them all on disk, or something bad happened..
         break
-    if fileitem.filename:
+    if fileitem.filename: # Check if file is not None
         # strip leading path from file name to avoid directory traversal attacks
-        fn = os.path.basename(fileitem.filename)
+        fn = os.path.basename(fileitem.filename) # Filename variable: complete path
         with open( '/home/pi/NewTorrents/' + fn , 'wb') as wfile:
             wfile.write(fileitem.file.read())
-        message += 'Upload {} Successful! <br>'.format(n+1)
+    # A message will be printed to inform user of successful/unsuccessful uploads
+        message += 'Upload {} Successful! <br>'.format(n+1) 
     else:
         message += 'Upload {} failed! :-(<br>'.format(n+1)
-end = time.time()
+end = time.time()  # Stop counting and render the page!
 print "Content-Type: text/html; charset=UTF-8"
 print """
 <html>
